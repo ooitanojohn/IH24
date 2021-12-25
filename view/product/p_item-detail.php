d2977a94a378872310b5ca5b68432de512e8ccf3
shuto niimi <71388489+ooitanojohn@users.noreply.github.com>
2021-12-25 12:07:00 +0900

    first commit

---
+++ view/product/p_item-detail.php
@@ -0,0 +1,215 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../../css/destyle.css">
+  <link rel="stylesheet" href="../../css/p_item-detail.css">
+  <title>Document</title>
+</head>
+
+<body>
+  <main>
+    <article>
+      <a href=""><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="chevron-left" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" style="width: 24px;height: 24px;">
+          <path fill="currentColor" d="M34.52 239.03L228.87 44.69c9.37-9.37 24.57-9.37 33.94 0l22.67 22.67c9.36 9.36 9.37 24.52.04 33.9L131.49 256l154.02 154.75c9.34 9.38 9.32 24.54-.04 33.9l-22.67 22.67c-9.37 9.37-24.57 9.37-33.94 0L34.52 272.97c-9.37-9.37-9.37-24.57 0-33.94z"></path>
+        </svg></a>
+      <ul>
+        <li><img alt="小さい写真"></li>
+      </ul>
+      <p><img alt="大きい写真"></p>
+      <div>
+        <section>
+          <div>
+            <h1>gyugyu</h1>
+          </div>
+          <div>
+            <p>1000*500</p>
+          </div>
+          <div>
+            <p>￥1,030 (税込み) 送料込み</p>
+          </div>
+          <div>
+            <div>
+              <a href="">
+                <p>いいね</p><svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="heart" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" style="height: 24px;width: 24px;">
+                  <path fill="currentColor" d="M458.4 64.3C400.6 15.7 311.3 23 256 79.3 200.7 23 111.4 15.6 53.6 64.3-21.6 127.6-10.6 230.8 43 285.5l175.4 178.7c10 10.2 23.4 15.9 37.6 15.9 14.3 0 27.6-5.6 37.6-15.8L469 285.6c53.5-54.7 64.7-157.9-10.6-221.3zm-23.6 187.5L259.4 430.5c-2.4 2.4-4.4 2.4-6.8 0L77.2 251.8c-36.5-37.2-43.9-107.6 7.3-150.7 38.9-32.7 98.9-27.8 136.5 10.5l35 35.7 35-35.7c37.8-38.5 97.8-43.2 136.5-10.6 51.1 43.1 43.5 113.9 7.3 150.8z"></path>
+                </svg>
+              </a>
+            </div>
+            <div>
+              <a href="">
+                <svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="comment-alt" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" style="height: 24px;width: 24px;">
+                  <path fill="currentColor" d="M448 0H64C28.7 0 0 28.7 0 64v288c0 35.3 28.7 64 64 64h96v84c0 7.1 5.8 12 12 12 2.4 0 4.9-.7 7.1-2.4L304 416h144c35.3 0 64-28.7 64-64V64c0-35.3-28.7-64-64-64zm16 352c0 8.8-7.2 16-16 16H288l-12.8 9.6L208 428v-60H64c-8.8 0-16-7.2-16-16V64c0-8.8 7.2-16 16-16h384c8.8 0 16 7.2 16 16v288z"></path>
+                </svg>
+                <p>2</p>
+              </a>
+            </div>
+          </div>
+          <div>
+            <a href="">商品の編集</a>
+          </div>
+        </section>
+        <section>
+          <h3>商品の説明</h3>
+          <p>otto</p>
+          <p>更新日時</p>
+        </section>
+        <section>
+          <h3>商品の情報</h3>
+          <ul>
+            <li>
+              <p>カテゴリー</p>
+              <ul>
+                <li><a href="">レディース</li>
+                <li><a href="">レディース</li>
+                <li><a href="">レディース</li>
+              </ul>
+            </li>
+            <li>
+              <p>商品サイズ</p>
+              <p>1000*500</p>
+            </li>
+            <li>
+              <p>商品の状態</p>
+              <p>新品、未使用</p>
+            </li>
+            <li>
+              <p>配送料の負担</p>
+              <p>送料込み(出品者負担)</p>
+            </li>
+            <li>
+              <p>配送の方法</p>
+              <p>未定</p>
+            </li>
+            <li>
+              <p>発送元の地域</p>
+              <p>山梨県</p>
+            </li>
+            <li>
+              <p>発送までの日数</p>
+              <p>2~3日で発送</p>
+            </li>
+          </ul>
+          <div>
+            <a href="">
+              <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="lock" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" style="width: 24px;height: 24px;">
+                <path fill="currentColor" d="M400 224h-24v-72C376 68.2 307.8 0 224 0S72 68.2 72 152v72H48c-26.5 0-48 21.5-48 48v192c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V272c0-26.5-21.5-48-48-48zm-104 0H152v-72c0-39.7 32.3-72 72-72s72 32.3 72 72v72z"></path>
+              </svg>
+              <div>
+                <p>メルカリ安心への取り組み</p>
+                <p>お金は事務局に支払われ、評価後に取り組みます</p>
+              </div>
+              <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="chevron-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" style="width: 24px;height: 24px;">
+                <path fill="currentColor" d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z"></path>
+              </svg>
+            </a>
+          </div>
+        </section>
+        <section>
+          <h3>出品者</h3>
+          <div>
+            <img alt="">
+            <div>
+              <p>SYUTO NIIMI</p>
+              <svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="id-card" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" style="width: 24px;height: 24px;">
+                <path fill="currentColor" d="M528 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h480c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48zm0 400H303.2c.9-4.5.8 3.6.8-22.4 0-31.8-30.1-57.6-67.2-57.6-10.8 0-18.7 8-44.8 8-26.9 0-33.4-8-44.8-8-37.1 0-67.2 25.8-67.2 57.6 0 26-.2 17.9.8 22.4H48V144h480v288zm-168-80h112c4.4 0 8-3.6 8-8v-16c0-4.4-3.6-8-8-8H360c-4.4 0-8 3.6-8 8v16c0 4.4 3.6 8 8 8zm0-64h112c4.4 0 8-3.6 8-8v-16c0-4.4-3.6-8-8-8H360c-4.4 0-8 3.6-8 8v16c0 4.4 3.6 8 8 8zm0-64h112c4.4 0 8-3.6 8-8v-16c0-4.4-3.6-8-8-8H360c-4.4 0-8 3.6-8 8v16c0 4.4 3.6 8 8 8zm-168 96c35.3 0 64-28.7 64-64s-28.7-64-64-64-64 28.7-64 64 28.7 64 64 64z"></path>
+              </svg>
+            </div>
+            <div>
+              <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="user-shield" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" style="width: 24px;height: 24px;">
+                <path fill="currentColor" d="M622.3 271.1l-115.2-45c-4.1-1.6-12.6-3.7-22.2 0l-115.2 45c-10.7 4.2-17.7 14-17.7 24.9 0 111.6 68.7 188.8 132.9 213.9 9.6 3.7 18 1.6 22.2 0C558.4 489.9 640 420.5 640 296c0-10.9-7-20.7-17.7-24.9zM496 462.4V273.3l95.5 37.3c-5.6 87.1-60.9 135.4-95.5 151.8zM224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm96 40c0-2.5.8-4.8 1.1-7.2-2.5-.1-4.9-.8-7.5-.8h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c6.8 0 13.3-1.5 19.2-4-54-42.9-99.2-116.7-99.2-212z"></path>
+              </svg>
+              <p>本人確認前</p>
+            </div>
+            <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="chevron-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" style="width: 24px;height: 24px;">
+              <path fill="currentColor" d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z"></path>
+            </svg>
+          </div>
+        </section>
+        <section>
+          <h3>コメント</h3>
+          <p>2件</p>
+          <ul>
+            <li>
+              <a href=""><img alt="プロフィール画像"></a>
+              <div>
+                <a href="">SYUTO NIIMI</a>
+                <div>
+                  <p>こんにちは</p>
+                  <div>
+                    <p>18時間前</p>
+                    <div>
+                      <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="flag" class="svg-inline--fa fa-flag fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" style="width: 24px;height: 24px;">
+                        <path fill="currentColor" d="M349.565 98.783C295.978 98.783 251.721 64 184.348 64c-24.955 0-47.309 4.384-68.045 12.013a55.947 55.947 0 0 0 3.586-23.562C118.117 24.015 94.806 1.206 66.338.048 34.345-1.254 8 24.296 8 56c0 19.026 9.497 35.825 24 45.945V488c0 13.255 10.745 24 24 24h16c13.255 0 24-10.745 24-24v-94.4c28.311-12.064 63.582-22.122 114.435-22.122 53.588 0 97.844 34.783 165.217 34.783 48.169 0 86.667-16.294 122.505-40.858C506.84 359.452 512 349.571 512 339.045v-243.1c0-23.393-24.269-38.87-45.485-29.016-34.338 15.948-76.454 31.854-116.95 31.854z"></path>
+                      </svg>
+                      <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="trash" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" style="width: 24px;height: 24px;">
+                        <path fill="currentColor" d="M432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16zM53.2 467a48 48 0 0 0 47.9 45h245.8a48 48 0 0 0 47.9-45L416 128H32z"></path>
+                      </svg>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </li>
+          </ul>
+          <h4>商品へのコメント</h4>
+          <form method="post">
+            <textarea placeholder="コメントする"></textarea>
+            <p>相手のことを考え丁寧なコメントを心がけましょう。不快な言葉遣いなどは利用制限や退会処分となる事があります</p>
+            <button type="submit">コメントを送信する</button>
+          </form>
+        </section>
+      </div>
+      <section>
+        <h3>この商品を見ている人におすすめ</h3>
+        <ul>
+          <li>
+            <div>
+              <img alt="商品画像">
+              <p>￥8,100</p>
+            </div>
+            <p>フィギュア</p>
+          </li>
+          <li>
+            <div>
+              <img alt="商品画像">
+              <p>￥8,100</p>
+            </div>
+            <p>フィギュア</p>
+          </li>
+          <li>
+            <div>
+              <img alt="商品画像">
+              <p>￥8,100</p>
+            </div>
+            <p>フィギュア</p>
+          </li>
+          <li>
+            <div>
+              <img alt="商品画像">
+              <p>￥8,100</p>
+            </div>
+            <p>フィギュア</p>
+          </li>
+          <li>
+            <div>
+              <img alt="商品画像">
+              <p>￥8,100</p>
+            </div>
+            <p>フィギュア</p>
+          </li>
+          <li>
+            <div>
+              <img alt="商品画像">
+              <p>￥8,100</p>
+            </div>
+            <p>フィギュア</p>
+          </li>
+        </ul>
+      </section>
+    </article>
+  </main>
+</body>
+
+</html>
